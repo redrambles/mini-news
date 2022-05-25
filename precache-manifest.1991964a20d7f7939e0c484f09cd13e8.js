234feb75c5baffee6db221a0f40cb4e355ab822e
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2022-05-25 15:58:53 -0400

    Updates

---
+++ precache-manifest.1991964a20d7f7939e0c484f09cd13e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e13742a9867f432dacce49cbe4f5f4",
+    "url": "/mini-news/index.html"
+  },
+  {
+    "revision": "693d80f132f6926e1ed4",
+    "url": "/mini-news/static/css/main.b65ce891.chunk.css"
+  },
+  {
+    "revision": "caa3f58cade5eb017dbf",
+    "url": "/mini-news/static/js/2.b8deaad7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mini-news/static/js/2.b8deaad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "693d80f132f6926e1ed4",
+    "url": "/mini-news/static/js/main.8cf35e08.chunk.js"
+  },
+  {
+    "revision": "6b6bf40601c96b00902b",
+    "url": "/mini-news/static/js/runtime-main.fc3548ab.js"
+  }
+]);
